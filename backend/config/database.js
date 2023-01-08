aba491aa4222fb978d2eefd7c4b691ea2a6bf630
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-01-08 21:47:00 +0300

    1 part done

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,19 @@ import mysql from "mysql2";
   
 // create the connection to database
 const db = mysql.createConnection({
-  host: 'fullstack',
-  user: 'root',
-  password: 'wtpfhm123',
-  database: 'pos_db'
+host : '127.0.0.1',
+port: '3306',
+user : 'root',
+password : 'wtpfhm123',
+database: 'pos_db'
+});
+
+db.connect( function (err) { 
+    if (err) { 
+        console.log("!!! Cannot connect !!! Error:"); throw err; 
+    } else { 
+        console.log("Connection established."); 
+    } 
 });
  
 export default db;
